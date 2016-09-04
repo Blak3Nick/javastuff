858eeeb07edeeb2070c84e83c2b2b73a078719e8
Blak3Nick <blaken2021@gmail.com>
2016-09-03 20:28:18 -0400

    new helloworld file and new print function

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Cyr1lfiggus1 on 9/3/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
