a1ac948e4e37716a89ac9a5faab3ec0e2061bdb9
Blak3Nick <blaken2021@gmail.com>
2016-09-03 20:46:23 -0400

    adding some various types to things messing with java

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,10 +1,36 @@
 package com.example.helloworld;
 
-/**
- * Created by Cyr1lfiggus1 on 9/3/2016.
- */
 public class HelloWorld {
+
+    static final double PINUM = 3.141529;
+    static String randomString = "String to Print";
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(randomString);
+        System.out.println(PINUM);
+        int integerOne = 22;
+        int integerTwo = integerOne + 1;
+        System.out.println(integerTwo);
+        byte bigByte = 127;
+        short bigShort = 32767;
+        int bigInt = 210000000;
+        long bigLong = 920000000000L;
+        float bigFloat = 3.14F;
+        double bigDouble = 3.14823;
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+
+        boolean trueOrFalse = true;
+
+        char randomChar = 65;
+        char anotherChar = 'A';
+        System.out.println(randomChar);
+        char escapedChars = '\\';
+
+        String stuff = "stuff";
+        String addStrings = randomString + stuff;
+        System.out.println(addStrings);
+        String byteString = Byte.toString(bigByte);
+
+
     }
 }
