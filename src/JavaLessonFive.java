f0d0575fa4b8837bb59e263c180418b4997f5ac5
Blak3Nick <blaken2021@gmail.com>
2016-09-08 14:45:31 -0400

    working with global and local variables

--- src/JavaLessonFive.java
+++ src/JavaLessonFive.java
@@ -5,15 +5,25 @@ public class JavaLessonFive {
 
     public static void main(String[] args)
     {
-        addThem(1,2);
+        System.out.println(addThem(1,2));
     }
 
     public static int addThem(int a, int b)
     {
         double smallPI = 3.140; // Local Variable only available here inside
 
-        System.out.println(myPi);
+        int c = a + b;
 
-        return 1;
+        tryToChange(6);
+        return c;
+
+
     }
+
+    public static void tryToChange (int d )
+    {
+        d = d+1;
+        System.out.println(d);
+    }
+
 }
