28029adaa1a0f2d2c834716b5974dca0640ddc6c
Blak3Nick <blaken2021@gmail.com>
2016-09-09 17:36:10 -0400

    going over various exceptions and errors

--- src/JavaLessonSix.java
+++ src/JavaLessonSix.java
@@ -1,11 +1,19 @@
+import java.util.*;
+import java.io.*;
 public class JavaLessonSix
 {
 
+    static Scanner userInput = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-
-        divideByZero(2);
-
+//        System.out.print("How old are you?");
+//        int age = checkValidAge();
+//        //divideByZero(2);
+//        if (age != 0) {
+//            System.out.println("You are " + age + "years old30");
+//        }
+            getAFile("IntegerArray.txt");
     }
     public static void divideByZero(int a ) {
         try
@@ -19,6 +27,26 @@ public class JavaLessonSix
             System.out.println(e .getMessage());
 
             System.out.println(e .toString());
+        }
+    }
+
+    public static int checkValidAge(){
+        try {
+            return userInput.nextInt();
+        }
+        catch (InputMismatchException e) {
+            userInput.next();
+            System.out.println("Nope");
+            return 0;
+        }
+    }
+    public static void getAFile(String fileName) {
+        try {
+        FileInputStream file = new FileInputStream(fileName);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("Can't find file");
         }
     }
 }
