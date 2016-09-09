33611a022e24f7bec60e72dcab74ffa2a5c87661
Blak3Nick <blaken2021@gmail.com>
2016-09-09 17:23:51 -0400

    going over various exceptions and errors

---
+++ src/JavaLessonSix.java
@@ -0,0 +1,24 @@
+public class JavaLessonSix
+{
+
+    public static void main(String[] args) {
+
+
+        divideByZero(2);
+
+    }
+    public static void divideByZero(int a ) {
+        try
+        {
+            System.out.println(a/0);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("Nope");
+
+            System.out.println(e .getMessage());
+
+            System.out.println(e .toString());
+        }
+    }
+}
