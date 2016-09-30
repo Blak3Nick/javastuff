922e3fb5183dfa0d7e0a7b420f5b50d93f1ff435
Blak3Nick <blaken2021@gmail.com>
2016-09-30 13:56:23 -0400

    Working with arrays in Java

---
+++ src/JavaLessonNine.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class JavaLessonNine {
+    public static void main(String[] args)
+    {
+
+        int[] randomArray;
+
+        int[] numberArray = new int[10];
+
+        randomArray = new int[20];
+
+        randomArray[1] = 2;
+
+        String[] stringArray = {"just", "random", "words"};
+
+        for(int i = 0; i < numberArray.length; i++)
+        {
+            numberArray[i] = i;
+        }
+    }
+}
