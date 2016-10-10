1f358dbd10a7239474cf0b2e13cae592e3881ae2
Blak3Nick <blaken2021@gmail.com>
2016-10-10 16:07:18 -0400

    Working with arrays in Java

--- src/LessonEleven.java
+++ src/LessonEleven.java
@@ -36,5 +36,52 @@ public class LessonEleven {
             System.out.println(names.get(i));
         }
 
+        System.out.println(names);
+
+        for(String i : names)
+        {
+            System.out.println(i);
+        }
+
+        Iterator indivItems = names.iterator();
+
+        while(indivItems.hasNext())
+        {
+            System.out.println(indivItems.next());
+        }
+
+        ArrayList nameCopy = new ArrayList();
+        ArrayList nameBackup = new ArrayList();
+
+        nameCopy.addAll(names);
+
+        String paulYoung = "PAul Young";
+
+        names.add(paulYoung);
+
+        if(names.contains(paulYoung))
+        {
+            System.out.println("Paul is here");
+        }
+
+        if (names.containsAll(nameCopy))
+        {
+            System.out.println("Errything is here");
+        }
+        names.clear();
+        if (names.isEmpty())
+        {
+            System.out.println("EMteeee");
+        }
+
+        Object[] moreNames = new Object[4];
+        moreNames = nameCopy.toArray();
+
+        System.out.println(moreNames);
     }
+
+
+
+
+
 }
