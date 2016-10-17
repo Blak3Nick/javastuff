71d35efe78b4aa9abf33b2d024dd10fa45d03fe9
Blak3Nick <blaken2021@gmail.com>
2016-10-17 12:51:18 -0400

    Working with Strings

---
+++ src/JavaLesson13.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Cyr1lfiggus1 on 10/17/2016.
+ */
+public class JavaLesson13 {
+
+
+    public static void main(String[] args) {
+        String randomString = "Im a string";
+
+        String havtaQuote = "He said, \"I'm a string inside\"";
+
+        int numTwo = 2;
+
+        System.out.println(randomString  + " " + havtaQuote);
+    }
+}
