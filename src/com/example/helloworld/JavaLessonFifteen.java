bd4d53a1f367ea9f8e2ef01eb32c24e4c0c59f63
Blak3Nick <blaken2021@gmail.com>
2016-11-28 11:43:22 -0500

    Working with objects

---
+++ src/com/example/helloworld/JavaLessonFifteen.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+/**
+ * Created by Cyr1lfiggus1 on 11/28/2016.
+ */
+public class JavaLessonFifteen {
+
+    public interface Drivable {
+
+        double PI = 3.14;
+
+        int getWheel();
+
+        void setWheels(int numWheels);
+
+        double getSpeed();
+
+
+
+
+
+    }
+}
---
+++ src/com/example/helloworld/LessonFifteen.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+public class LessonFifteen {
+
+    public static void main(String[] args){
+
+        Vehicle car = new Vehicle(4, 100.00);
+
+        System.out.println("The car's max speed "+ car.getSpeed());
+        System.out.println("The car's wheels are "+ car.getWheel());
+    }
+
+
+
+}
